741967139a285d82a15ede7326bebe043fae4a04
Dawid Pasek <d.pasek@striped-giraffe.com>
2023-10-27 18:39:49 +0200

    Add company_model

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/idekpas/xm-company
 
 go 1.21.3
+
+require github.com/google/uuid v1.4.0
---
+++ internal/xmcompany/company_model.go
@@ -0,0 +1,29 @@
+package xmcompany
+
+import "github.com/google/uuid"
+
+type CompanyType string
+
+const (
+	Corporations       CompanyType = "Corporations"
+	NonProfit          CompanyType = "NonProfit"
+	Cooperative        CompanyType = "Cooperative"
+	SoleProprietorship CompanyType = "Sole Proprietorship"
+)
+
+func (ct CompanyType) IsValid() bool {
+	switch ct {
+	case Corporations, NonProfit, Cooperative, SoleProprietorship:
+		return true
+	}
+	return false
+}
+
+type Company struct {
+	ID              uuid.UUID   `db:"id"`
+	Name            string      `db:"name"`
+	Description     string      `db:"description"`
+	EmployeesAmount int         `db:"employees_amount"`
+	Registered      bool        `db:"registered"`
+	Type            CompanyType `db:"type"`
+}
